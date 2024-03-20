b1dc2e5b191d27b725da5a93075c6f7c660cd2a0
Mileshaase <Mileshaase.imperium@gmail.com>
2024-03-20 18:36:32 -0400

    PostLecture

--- Assets/Scripts/LightSaber.cs
+++ Assets/Scripts/LightSaber.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LightSaber : MonoBehaviour
 {
-    public GameObject blade;
+    private GameObject blade;
     public Light baseLight;
     public Transform FiringPoint;
 
@@ -16,11 +16,12 @@ public class LightSaber : MonoBehaviour
 
     private bool isActive = false;
 
-    //private GameObject currentBlade;
+    private GameObject currentBlade;
 
-    public void Start()
+    // Start is called before the first frame update
+    void Start()
     {
-        lightColors =  new List<Color>
+        lightColors = new List<Color>
         {
             Color.blue,
             Color.green,
@@ -28,47 +29,95 @@ public class LightSaber : MonoBehaviour
             Color.red
         };
 
-        // blade = Resources.Load<GameObject>("Blade");
-        // blade = Resources.Load<GameObject>("Blade Disk");
-
-        //FiringPoint.position = new Vector3(FiringPoint.position.x, FiringPoint.position.y - 0.3f, FiringPoint.position.z);
+        blade = Resources.Load<GameObject>("BladeDisk");
     }
 
-    public void Update()
+    // Update is called once per frame
+    void Update()
     {
-        if(Activated && !isActive)
-        {
-            Debug.Log("Activated");
-            isActive = true;
-            GameObject Blade = Instantiate(blade, FiringPoint.position, blade.transform.rotation);
-            Blade.transform.parent = FiringPoint;
-            Blade.GetComponent<MeshRenderer>().material = colors[colorChoice];
-            baseLight.enabled = true;
-            baseLight.color = lightColors[colorChoice];
-        }
-        if(!Activated && isActive)
-        {
-            Debug.Log("Deactivate");
-            if(baseLight.enabled)
-            {
-                baseLight.enabled = false;
-            }
-            if(FiringPoint.childCount > 0)
-            {
-                Debug.Log(FiringPoint.GetChild(0).gameObject.name);
-                Destroy(FiringPoint.GetChild(0).gameObject);
-            }
-            isActive = false;
-        }      
+        // if (Activated && !isActive)
+        // {
+        //     Debug.Log("Activated");
+        //     isActive = true;
 
-        // if (Activated && currentBlade == null)
-        // {
-        //     ActivateBlade();
+        //     GameObject Blade = Instantiate(blade, FiringPoint.position, blade.transform.rotation);
+        //     Blade.transform.parent = FiringPoint;
+        //     Blade.GetComponent<MeshRenderer>().material = colors[colorChoice];
+
+        //     baseLight.enabled = true;
+        //     baseLight.color = lightColors[colorChoice];
         // }
-        // else if (!Activated && currentBlade != null)
+        // if(!Activated && isActive)
         // {
-        //     DeactivateBlade();
-        // } 
+        //     Debug.Log("Deactivated");
+        //     if(baseLight.enabled)
+        //     {
+        //         baseLight.enabled = false;
+        //     }
+        //     if(FiringPoint.childCount > 0)
+        //     {
+        //         Debug.Log(FiringPoint.GetChild(0).gameObject.name);
+        //         Destroy(FiringPoint.GetChild(0).gameObject);
+        //     }
+
+        //     isActive = false;
+        // }
+
+        if(Activated && currentBlade == null)
+        {
+            StartCoroutine(ExpandBlade());
+        }
+        else if (!Activated && currentBlade != null)
+        {
+            DeactivateBlade();
+        }
+
+    }
+
+    private void ActivateBlade()
+    {
+        
+
+        
+    }
+
+    private IEnumerator ExpandBlade()
+    {
+        FiringPoint.position = new Vector3(FiringPoint.position.x, FiringPoint.position.y - 0.3f, FiringPoint.position.z);
+
+        currentBlade = Instantiate(blade, FiringPoint.position, blade.transform.rotation);
+        currentBlade.transform.parent = FiringPoint;
+        currentBlade.GetComponent<MeshRenderer>().material = colors[colorChoice];
+
+        baseLight.enabled = true;
+        baseLight.color = lightColors[colorChoice];
+
+        float duration = 0.5f;
+        float elapsedTime = 0f;
+
+        Vector3 initalScale = blade.transform.localScale;
+        Vector3 targetScale = new Vector3(0.03f, 0.3f, 0.03f);
+
+        currentBlade.transform.localScale = initalScale;
+        Vector3 endPos = new(blade.transform.position.x, blade.transform.position.y + 0.3f, blade.transform.position.z);
+
+        while(elapsedTime < duration)
+        {
+            currentBlade.transform.localPosition = Vector3.Lerp(blade.transform.position, endPos, elapsedTime/duration);
+            currentBlade.transform.localScale = Vector3.Lerp(initalScale, targetScale, elapsedTime/duration);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        currentBlade.transform.localPosition = endPos;
+        currentBlade.transform.localScale = targetScale;
+    }
+
+    private void DeactivateBlade()
+    {
+        baseLight.enabled = false;
+        Destroy(currentBlade);
+        currentBlade = null;
     }
 
     // public void Activate()
@@ -83,49 +132,5 @@ public class LightSaber : MonoBehaviour
     //     }
     // }
 
-    // public void Activate()
-    // {
-    //     Activated = !Activated;
-    // }
 
-    // private void ActivateBlade()
-    // {
-    //     currentBlade = Instantiate(blade, FiringPoint.position, blade.transform.rotation);
-    //     currentBlade.GetComponent<MeshRenderer>().material = colors[colorChoice];
-    //     currentBlade.transform.parent = FiringPoint;
-    //     baseLight.enabled = true;
-    //     baseLight.color = lightColors[colorChoice];
-    //     StartCoroutine(ExpandBlade());
-    // }
-
-    // private void DeactivateBlade()
-    // {
-    //     baseLight.enabled = false;
-    //     Destroy(currentBlade);
-    //     currentBlade = null;
-    // }
-
-    // private IEnumerator ExpandBlade()
-    // {
-    //     float duration = 0.5f; // Adjust the duration of the expansion
-    //     float elapsedTime = 0f;
-
-    //     Vector3 initialScale = blade.transform.localScale; // Initial scale of the blade
-    //     Vector3 targetScale = new Vector3(0.03f, 0.3f, 0.03f); // Target scale of the blade
-
-    //     currentBlade.transform.localScale = initialScale;
-    //     Vector3 endPos = new(blade.transform.position.x, blade.transform.position.y + 0.3f, blade.transform.position.z);
-
-    //     while (elapsedTime < duration)
-    //     {
-    //         currentBlade.transform.localPosition = Vector3.Lerp(blade.transform.position, endPos, elapsedTime / duration);
-    //         currentBlade.transform.localScale = Vector3.Lerp(initialScale, targetScale, elapsedTime / duration);
-
-    //         elapsedTime += Time.deltaTime;
-    //         yield return null;
-    //     }
-
-    //     currentBlade.transform.localPosition = endPos; // Ensure it reaches the top
-    //     currentBlade.transform.localScale = targetScale; // Ensure it reaches the full length
-    // }
 }
